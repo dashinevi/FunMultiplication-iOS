658d9761d607e7784764d7e1159bc4922c56bad7
Diana Dashynevich <ddashinevich@gmail.com>
2025-02-21 15:35:56 +0100

    Initial Commit

---
+++ FunMultiplication/FunMultiplicationApp.swift
@@ -0,0 +1,17 @@
+//
+//  FunMultiplicationApp.swift
+//  FunMultiplication
+//
+//  Created by Diana Dashinevich on 21/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct FunMultiplicationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
